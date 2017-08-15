8d4223661fbf99b4f5dd4b675c2dfad43672a785
eenblam <ben.a.elam@gmail.com>
2017-08-14 18:15:30 -0700

    Catch second pair of ads

--- src/kill.js
+++ src/kill.js
@@ -2,8 +2,20 @@ function killNewsFeed() {
 	document.querySelector('[aria-label="News Feed"]').innerHTML = "News Feed slain!";
 }
 
-function killSponsoredAds() {
+function killEgoPane() {
 	document.getElementById("pagelet_ego_pane").innerHTML = "Sponsored ads slain!";
+}
+
+async function killSponsoredAds() {
+	// Thanks https://stackoverflow.com/a/39914235
+	killEgoPane();
+	await sleep(500);
+	// FB likes to buffer another pair of ads
+	killEgoPane();
+}
+
+function sleep(ms) {
+	  return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 killNewsFeed();
